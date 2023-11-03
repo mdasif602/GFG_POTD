c230231c52a660c4d9857a8a38fdfeaefcac60bd
MD ASIF HUSSAIN <mdasifhussain268@gmail.com>
2023-11-04 00:44:58 +0530

    Update Find Transition Point.cpp

--- Find Transition Point/Find Transition Point.cpp
+++ Find Transition Point/Find Transition Point.cpp
@@ -1,7 +1,15 @@
-for(int i = 0; i < n; i++) {
+class Solution
+{
+public:    
+    
+    int transitionPoint(int arr[], int n) {
+        // code here
+        for(int i = 0; i < n; i++) {
             if(arr[i] == 1) {
                 return i;
             }
         }
         
         return -1;
+    }
+};
