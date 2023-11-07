58919ae66bd0b0d6d7ffb18b622c2ccece149f52
MD ASIF HUSSAIN <mdasifhussain268@gmail.com>
2023-11-08 00:58:58 +0530

    Update Print Matrix in snake Pattern.java

--- Print Matrix in snake Pattern/Print Matrix in snake Pattern.java
+++ Print Matrix in snake Pattern/Print Matrix in snake Pattern.java
@@ -1,1 +1,26 @@
-
+class Solution
+{
+    //Function to return list of integers visited in snake pattern in matrix.
+    static ArrayList<Integer> snakePattern(int matrix[][])
+    {
+        // code here 
+        ArrayList<Integer> ans = new ArrayList<>();
+        int flag = 0;
+        for(int i = 0; i < matrix.length; i++){
+            if(flag == 0){
+                 for(int j = 0; j < matrix.length; j++){
+                    ans.add(matrix[i][j]);
+                }
+                flag = 1;
+            } else {
+                for(int j = matrix.length - 1; j >= 0; j--) {
+                    ans.add(matrix[i][j]);
+                }
+                flag = 0;
+            }
+            
+        }
+        
+        return ans;
+    }
+}
